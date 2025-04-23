3e7b8c54b22a473cdf6de583c5ebf7f8a6bcc010
AndreaCatapano <a.catapano96@gmail.com>
2025-04-23 14:17:28 +0200

    feat: linked PostContext in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,24 +7,25 @@ import Products from './pages/Products.jsx'
 import SingleProduct from './pages/SingleProduct.jsx'
 import DefaultLayout from './layout/DefaultLayout.jsx'
 
-
+import PostContext from './contexts/PostContext.jsx'
 
 
 function App() {
 
   return (
-    <>  <BrowserRouter>
-      <Routes>
-        <Route element={<DefaultLayout />}>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/products/:id' element={<SingleProduct />}></Route>
-        </Route>
-      </Routes>
-    </BrowserRouter>
-    </>
+    <PostContext.Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route element={<DefaultLayout />}>
+            <Route path='/' element={<Home />} />
+            <Route path='/about' element={<About />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/products/:id' element={<SingleProduct />}></Route>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </PostContext.Provider >
   )
 }
 
-export default App
+export default App;
